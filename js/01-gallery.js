bed434584f883ef3526f965dd397d4ea83a212c8
BondarenkoOl <bondarenko.ptl@gmail.com>
2022-09-22 16:28:14 +0300

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const gelleryPictyresItem = document.querySelector('.gallery');
+
+const imageInstance = ({ preview, original, description }) =>
+	`<div class="gallery__item">
+		<a class="gallery__link" href="${original}">
+			<img
+				class="gallery__image"
+				src="${preview}"
+				data-source="${original}"
+				alt="${description}"
+			/>
+		</a>
+	</div>`;
+
+const generatedImageList = () => {
+	const imageList = galleryItems.map(imageInstance).join('');
+	gelleryPictyresItem.insertAdjacentHTML('beforeend', imageList);
+};
+generatedImageList();
+
+gelleryPictyresItem.addEventListener('click', (evt) => {
+	evt.preventDefault();
+	if (evt.target.nodeName !== 'IMG') {
+		return;
+	}
+	const activImage = evt.target.dataset.source;
+	const instance = basicLightbox.create(`
+    <img src="${activImage}" width="800" height="600">
+`);
+	instance.show();
+
+	gelleryPictyresItem.addEventListener('keydown', (evt) => {
+		if (evt.key === 'Escape') {
+			instance.close();
+		}
+	});
+});
