61b5f1fad63b53f147291f0375698f4dbe5115f5
BondarenkoOl <bondarenko.ptl@gmail.com>
2022-09-24 14:33:27 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const gelleryPictyresItem = document.querySelector('.gallery');
+const imageInstance = galleryItems.reduce(
+	(acc, item) =>
+		acc +
+		`<a class="gallery__item" href="${item.original}">
+            <img class="gallery__image"
+            src="${item.preview}" 
+            alt="${item.description}"/>
+        </a>
+        `,
+	''
+);
+gelleryPictyresItem.insertAdjacentHTML('beforeend', imageInstance);
+console.log(imageInstance);
+
+new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionPosition: 'bottom',
+	captionDelay: 250
+});
